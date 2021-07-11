9156e0bedcc61981042c8a4ebaadfd02a3f3b06f
liupanfengGH <2582090092@qq.com>
2021-07-11 17:27:14 +0800

    修改 Jump 相关逻辑

--- DotweenGroupDemo/Assets/Editor/DotweenAnimationContrlEditor.cs
+++ DotweenGroupDemo/Assets/Editor/DotweenAnimationContrlEditor.cs
@@ -51,7 +51,28 @@ public class DotweenAnimationContrlEditor : Editor
     {
         EditorGUILayout.BeginHorizontal();
         Duration(sp);
+        SpeedBase(sp);
         EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.BeginHorizontal();
+        Delay(sp);
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.BeginHorizontal();
+        IgnoreTimeScale(sp);
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.BeginHorizontal();
+        int eNewValue = EaseType(sp);
+        EditorGUILayout.EndHorizontal();
+
+        EaseCurve(eNewValue, sp);
+
+        EditorGUILayout.BeginHorizontal();
+        int loopCount = Loops(sp);
+        EditorGUILayout.EndHorizontal();
+
+        LoopType(loopCount, sp);
 
         EditorGUILayout.BeginHorizontal();
         OptionalFloat0(sp,GUIContentKey.JumpPower,45f,0,100f);
