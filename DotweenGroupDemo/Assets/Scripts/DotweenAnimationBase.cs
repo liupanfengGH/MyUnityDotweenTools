4113c3cbdd2517d53868bdbb4b281459d4b5eb20
liupanfengGH <2582090092@qq.com>
2021-07-18 22:44:48 +0800

    调整逻辑结构！

---
+++ DotweenGroupDemo/Assets/Scripts/DotweenAnimationBase.cs
@@ -0,0 +1,123 @@
+﻿using DG.Tweening;
+using System;
+using UnityEngine;
+
+public abstract class DotweenAnimationBase : MonoBehaviour
+{
+    public DotweenAnimationData animationData;
+
+    public bool autoPlay;
+
+    [NonSerialized]
+    protected Tween tween = null;
+
+    void Start()
+    {
+        if (autoPlay)
+        {
+            Play();
+        }
+    }
+
+    public void Play()
+    {
+        CreateTween();
+        tween.Play();
+    }
+
+    protected virtual void CreateTween()
+    {
+        TweenBehaviour();
+        TweenSetValue();
+        TweenEvents();
+    }
+
+    protected abstract void TweenBehaviour();
+
+    protected virtual void TweenSetValue()
+    {
+        if (animationData.isFrom)
+        {
+            FromProcess();
+            var tweener = tween as Tweener;
+            if (null != tweener)
+            {
+                tweener.From(animationData.isRelative);
+            }
+            else
+            {
+                tween.SetRelative(animationData.isRelative);
+            }
+        }
+
+        tween.SetTarget(gameObject);
+        tween.SetDelay(animationData.delay);
+        tween.SetLoops(animationData.loops, animationData.loopType);
+        tween.SetAutoKill(animationData.autoKill);
+        tween.OnKill(() => { tween = null; });
+        tween.SetUpdate(animationData.isIgnoreTimeScale);
+        if (animationData.isSpeedBase)
+        {
+            tween.SetSpeedBased();
+        }
+        if (animationData.easeType == Ease.INTERNAL_Custom)
+        {
+            tween.SetEase(animationData.easeCurve);
+        }
+        else
+        {
+            tween.SetEase(animationData.easeType);
+        }
+    }
+
+    protected abstract void FromProcess();
+
+    protected virtual void TweenEvents()
+    {
+        if (animationData.hasStart && null != animationData.onStart)
+        {
+            tween.OnStart(animationData.onStart.Invoke);
+        }
+        if (animationData.hasPlay && null != animationData.onPlay)
+        {
+            tween.OnPlay(animationData.onPlay.Invoke);
+        }
+        if (animationData.hasUpdate && null != animationData.onUpdate)
+        {
+            tween.OnUpdate(animationData.onUpdate.Invoke);
+        }
+        if (animationData.hasStepComplete && null != animationData.onStepComplete)
+        {
+            tween.OnStepComplete(animationData.onStepComplete.Invoke);
+        }
+        if (animationData.hasComplete && null != animationData.onComplete)
+        {
+            tween.OnComplete(animationData.onComplete.Invoke);
+        }
+        if (animationData.hasRewind && null != animationData.onRewind)
+        {
+            tween.OnRewind(animationData.onRewind.Invoke);
+        }
+
+        tween.Pause();
+
+        if (animationData.hasCreated && null != animationData.onCreated)
+        {
+            animationData.onCreated.Invoke();
+        }
+    }
+
+    public void Stop()
+    {
+        if (null != tween)
+        {
+            tween.Rewind();
+            tween.Kill();
+            tween = null;
+            StopPostProcess();
+        }
+    }
+
+    protected abstract void StopPostProcess();
+
+}
--- DotweenGroupDemo/Assets/Scripts/DotweenAnimationLocalMove.cs
+++ DotweenGroupDemo/Assets/Scripts/DotweenAnimationLocalMove.cs
@@ -1,126 +1,19 @@
-﻿using UnityEngine;
-using System;
-using DG.Tweening;
+﻿using DG.Tweening;
 
-public class DotweenAnimationLocalMove : MonoBehaviour
+public class DotweenAnimationLocalMove : DotweenAnimationBase
 {
-    public DotweenAnimationData animationData;
-
-    public bool autoPlay;
-
-    [NonSerialized]
-    private Tween tween = null;
-
-    void Start()
-    {
-        if (autoPlay)
-        {
-            Play();
-        }
-    }
-
-    void OnEnable()
-    {
-        
-    }
-
-    void OnDisable()
-    {
-        
-    }
-
-    public void Play()
-    {
-        CreateTween(animationData);
-        tween.Play();
-    }
-
-    public void Stop()
-    {
-        if(null != tween)
-        {
-            tween.Rewind();
-            tween.Kill();
-            tween = null;
-            if (animationData.isFrom)
-            {
-                transform.localPosition = animationData.defulatValueV3;
-            }
-        }
-    }
-
-    private void CreateTween(DotweenAnimationData animationData)
+    protected override void FromProcess()
     {
         animationData.defulatValueV3 = transform.localPosition;
-        tween = transform.DOLocalMove(animationData.endValueV3, animationData.duration, animationData.optionalBool0);
-
-        if (null != tween)
-        {
-            if (animationData.isFrom)
-            {
-                var tweener = tween as Tweener;
-                if (null != tweener)
-                {
-                    tweener.From(animationData.isRelative);
-                }
-                else
-                {
-                    tween.SetRelative(animationData.isRelative);
-                }
-            }
-
-            tween.SetTarget(gameObject);
-            tween.SetDelay(animationData.delay);
-            tween.SetLoops(animationData.loops, animationData.loopType);
-            tween.SetAutoKill(animationData.autoKill);
-            tween.OnKill(() => { tween = null; });
-            tween.SetUpdate(animationData.isIgnoreTimeScale);
-            if (animationData.isSpeedBase)
-            {
-                tween.SetSpeedBased();
-            }
-            if (animationData.easeType == Ease.INTERNAL_Custom)
-            {
-                tween.SetEase(animationData.easeCurve);
-            }
-            else
-            {
-                tween.SetEase(animationData.easeType);
-            }
-
-            if (animationData.hasStart && null != animationData.onStart)
-            {
-                tween.OnStart(animationData.onStart.Invoke);
-            }
-            if (animationData.hasPlay && null != animationData.onPlay)
-            {
-                tween.OnPlay(animationData.onPlay.Invoke);
-            }
-            if (animationData.hasUpdate && null != animationData.onUpdate)
-            {
-                tween.OnUpdate(animationData.onUpdate.Invoke);
-            }
-            if (animationData.hasStepComplete && null != animationData.onStepComplete)
-            {
-                tween.OnStepComplete(animationData.onStepComplete.Invoke);
-            }
-            if (animationData.hasComplete && null != animationData.onComplete)
-            {
-                tween.OnComplete(animationData.onComplete.Invoke);
-            }
-            if (animationData.hasRewind && null != animationData.onRewind)
-            {
-                tween.OnRewind(animationData.onRewind.Invoke);
-            }
-
-            tween.Pause();
-
-            if (animationData.hasCreated && null != animationData.onCreated)
-            {
-                animationData.onCreated.Invoke();
-            }
-        }
     }
 
+    protected override void StopPostProcess()
+    {
+        transform.localPosition = animationData.defulatValueV3;
+    }
 
+    protected override void TweenBehaviour()
+    {
+        tween = transform.DOLocalMove(animationData.endValueV3, animationData.duration, animationData.optionalBool0);
+    }
 }
